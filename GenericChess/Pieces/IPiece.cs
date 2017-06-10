a3417d42eda5aeace53f8516b3a11eea2451878f
Eric Catlin <eric.catlin@indigoslate.com>
2017-06-10 12:14:16 -0700

    The IPiece interface for different piece types

---
+++ GenericChess/Pieces/IPiece.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericChess
+{
+    interface IPiece
+    {
+        Color color { get; set; }
+        Vector2 position { get; set; }
+        bool IsMoveValid(Board board, Vector2 end_pos);
+    }
+    public enum Color
+    {
+        White,
+        Black
+    }
+}
