a7b98d578e156b968e4d5c4c5d6188442efe7bc6
Eric Catlin <eric.catlin@indigoslate.com>
2017-06-10 20:11:53 -0700

    Added overload to Vector2.AddVector to take x,y ints

--- GenericChess/Chess/Vector2.cs
+++ GenericChess/Chess/Vector2.cs
@@ -17,20 +17,29 @@ namespace GenericChess
             this.y = y;
         }
 
+        //Gets the difference between this vector and another
         public Vector2 Delta(Vector2 other)
         {
             return new Vector2(other.x - this.x, other.y - this.y);
         }
 
+        //Determines if 2 vectors are equivelent
         public bool isEqual(Vector2 other)
         {
             var delta = Delta(other);
             return (delta.x == 0 && delta.y == 0);
         }
 
+        //Adds a vector to this vector and returns the result
         public Vector2 AddVector(Vector2 point)
         {
             return new Vector2(this.x + point.x, this.y + point.y);
+        }
+
+        //overloaded function to reduce complexity of lots of other functions
+        public Vector2 AddVector(int x, int y)
+        {
+            return new Vector2(this.x + x, this.y + y);
         }
     }
 }
